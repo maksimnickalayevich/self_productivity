0736c6da11b00e71776c19788fb25434d12d9fd9
maksimnickalayevich <maksim.nickalayevich@deliverect.com>
2024-01-01 13:03:00 +0100

    refactor with encapsulation

--- src/configs/db_config.rs
+++ src/configs/db_config.rs
@@ -8,11 +8,22 @@ pub struct DbConfig {
     db_username: String,
     db_password: String,
     db_name: String,
-    pub db_max_connections: u32,
-    pub connection_url: Option<String>
+    db_max_connections: u32,
+    connection_url: Option<String>
 }
 
 impl DbConfig {
+    fn compose_connection_url(&self) -> String {
+        return format!(
+            "postgres://{}:{}@{}:{}/{}",
+             self.db_username, 
+             self.db_password,
+             self.db_host,
+             self.db_port,
+             self.db_name
+        );
+    }
+
     pub fn from_env() -> DbConfig {
         let _cfg_builder = config::Config::builder()
             .add_source(config::Environment::default());
@@ -27,27 +38,18 @@ impl DbConfig {
         return config;
     }
 
-    fn compose_connection_url(&mut self) {
-        let conn_url = Some(format!(
-            "postgres://{}:{}@{}:{}/{}",
-             self.db_username, 
-             self.db_password,
-             self.db_host,
-             self.db_port,
-             self.db_name
-            )
-        );
-        self.connection_url = conn_url
-    }
-
-    pub fn get_connection_url(&mut self) -> &str {
-        self.compose_connection_url();
-        match &self.connection_url {
+    pub fn get_connection_url(&self) -> &str {
+        return match &self.connection_url {
             Some(url) => url,
             _ => panic!("No connection url found in DbConfig {:?}", self)
         }
     }
     
+    pub fn set_connection_url(&mut self) {
+        let conn_url: String = self.compose_connection_url();
+        self.connection_url = Some(conn_url);
+    }
+
     pub fn get_max_connections(&self) -> u32 {
         self.db_max_connections
     }
